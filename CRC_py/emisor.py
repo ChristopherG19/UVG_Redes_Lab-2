e45ac90ac690515ee80715368864e6f878cd32a1
Isabel Solano <misolanob@gmail.com>
2023-08-01 00:00:31 -0600

    Feature(Emisor): CRC emisor side

--- CRC_py/emisor.py
+++ CRC_py/emisor.py
@@ -2,12 +2,12 @@ from view import *
 
 class Emisor():
 
-    def __init__(self, input):
-        input = input
+    def __init__(self, userInfo):
+        userInfo = userInfo
 
         self.trama = []
 
-        for char in input:
+        for char in userInfo:
             if char == '1':
                 self.trama.append(True)
 
@@ -25,6 +25,67 @@ class Emisor():
 
         print("\ncon padding")
         printTrama(self.trama)
+
+
+        # generate polinomio
+        self.polinomio = [
+            True, False, True, True, False, True, False, True, 
+            False, False, False, True, False, True, False, True,
+            False, False, False, False, True, True, True, False, 
+            True, False, False, True, True, False, True, False
+        ]
+
+        self.CRC()
+
+    def CRC(self):
+        actual = []
+
+        # initialize by coping the first 32 bites
+        for i in range(len(self.polinomio)):
+            actual.append(self.trama[i])
+
+        EOL = len(self.polinomio)
+
+        while(EOL < len(self.trama)):
+
+            temp = []
+
+            print()
+            printTrama(actual)
+            printTrama(self.polinomio)
+
+            for i in range(len(self.polinomio)):
+                temp.append(actual[i] ^ self.polinomio[i])
+
+            printTrama(temp)
+
+            # pass the zeros
+            while(not temp[0] and EOL < len(self.trama)):
+                temp.append(self.trama[EOL])
+                EOL += 1
+
+                temp.pop()
+
+            # replace actual for temporal
+            actual = temp
+
+        print("\nActual")
+        printTrama(actual)
+        actual.pop()
+        printTrama(actual)
+
+        # Add the result to the original trama
+        self.result = []
+        for b in self.tramaOriginal:
+            self.result.append(b)
+        for b in actual:
+            self.result.append(b)
+
+        print("\nFinal result")
+        printTrama(self.result)
+        
+
+
         
 
         
--- CRC_py/main.py
+++ CRC_py/main.py
@@ -1,4 +1,6 @@
 from view import *
+from emisor import *
+from receptor import *
 
 def main():
     
@@ -6,6 +8,10 @@ def main():
     userInput = getUserInput()
     print("Input")
     print(userInput)
+
+    
+    emisor = Emisor(userInput)
+
 
 
 if __name__ == '__main__':
